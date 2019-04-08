c24560cd4da0f6058e573595154d5e554e913c5c
Tomvyc <tomas.vycas111@gmail.com>
2019-04-08 17:16:57 +0300

    Add ab AsyncTask for calculations

--- app/src/main/java/com/example/coinscounter/EdgeFinder.java
+++ app/src/main/java/com/example/coinscounter/EdgeFinder.java
@@ -1,50 +1,32 @@
 package com.example.coinscounter;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class EdgeFinder {
 
-    private final Bitmap IMG;
+    private static final String TAG = "EdgeFinder";
+    private Bitmap img;
+    private MainActivity main;
 
-    public EdgeFinder(Bitmap image) {
-        this.IMG = image;
+    public EdgeFinder(Bitmap image,  MainActivity main) {
+        this.img = image;
+        this.main = main;
+//        new BackgroundImageResize(img);
     }
 
-    public Bitmap turnMonochrome() {
-        int width = IMG.getWidth();
-        int height = IMG.getHeight();
-        // create output bitmap
-        Bitmap bmOut = Bitmap.createBitmap(width, height, IMG.getConfig());
-        // color information
-        int A, R, G, B;
-        int pixel;
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                // get pixel color
-                pixel = IMG.getPixel(x, y);
-                A = Color.alpha(pixel);
-                R = Color.red(pixel);
-                G = Color.green(pixel);
-                B = Color.blue(pixel);
-                int gray = (int) (0.2989 * R + 0.5870 * G + 0.1140 * B);
-                // use 128 as threshold, above -> white, below -> black
-                if (gray > 128) {
-                    gray = 255;
-                } else {
-                    gray = 0;
-                }
-                // set new pixel color to output bitmap
-                bmOut.setPixel(x, y, Color.argb(A, gray, gray, gray));
-            }
-        }
 
-        return bmOut;
+    public void setBitmap(Bitmap bitmap){
+        this.img = bitmap;
     }
 
 }
--- app/src/main/java/com/example/coinscounter/MainActivity.java
+++ app/src/main/java/com/example/coinscounter/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.coinscounter;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -13,10 +15,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     String currentPhotoPath;
     ImageView imgView;
+    TextView text;
 
 
     @Override
@@ -33,14 +38,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imgView = findViewById(R.id.imageView);
+        text = findViewById(R.id.textView);
     }
 
-
     public void openCamera(View view) {
+        imgView.setVisibility(View.INVISIBLE);
         dispatchTakePictureIntent();
-        Bitmap myBitmap = BitmapFactory.decodeFile(currentPhotoPath);
-//        imgView.setImageBitmap(myBitmap);
-//        imgView.setAlpha(1);
     }
 
     private void dispatchTakePictureIntent() {
@@ -85,10 +88,98 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bitmap takenImage = BitmapFactory.decodeFile(currentPhotoPath);
-            EdgeFinder edgeFinder = new EdgeFinder(takenImage);
-            imgView.setImageBitmap(edgeFinder.turnMonochrome());
+            new ImageProcessing(this).execute(takenImage);
         }else{
             Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private static class ImageProcessing extends AsyncTask<Bitmap, String, Bitmap> {
+        private WeakReference<MainActivity> activityWeakReference;
+
+        ImageProcessing(MainActivity activity) {
+            activityWeakReference = new WeakReference<MainActivity>(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+
+            activity.text.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Bitmap... bitmaps) {
+            publishProgress("Starting to compress...");
+            bitmaps[0] = Bitmap.createScaledBitmap(bitmaps[0], 700,700,false);
+            publishProgress("Turning monochrome...");
+            bitmaps[0] = turnMonochrome(bitmaps[0]);
+            return bitmaps[0];
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+
+            activity.text.setText(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+
+            activity.imgView.setImageBitmap(bitmap);
+            activity.imgView.setVisibility(View.VISIBLE);
+
+            activity.text.setText("");
+            activity.text.setVisibility(View.INVISIBLE);
+        }
+
+        public Bitmap turnMonochrome(Bitmap img) {
+            Log.d(TAG, "Turning image to monochrome");
+            int width = img.getWidth();
+            int height = img.getHeight();
+            // create output bitmap
+            Bitmap bmOut = Bitmap.createBitmap(width, height, img.getConfig());
+            // color information
+            int A, R, G, B;
+            int pixel;
+            for (int x = 0; x < width; ++x) {
+                for (int y = 0; y < height; ++y) {
+                    // get pixel color
+                    pixel = img.getPixel(x, y);
+                    A = Color.alpha(pixel);
+                    R = Color.red(pixel);
+                    G = Color.green(pixel);
+                    B = Color.blue(pixel);
+                    int gray = (int) (0.2989 * R + 0.5870 * G + 0.1140 * B);
+                    // use 128 as threshold, above -> white, below -> black
+                    if (gray > 128) {
+                        gray = 255;
+                    } else {
+                        gray = 0;
+                    }
+                    // set new pixel color to output bitmap
+                    bmOut.setPixel(x, y, Color.argb(A, gray, gray, gray));
+                }
+            }
+
+            return bmOut;
         }
     }
 }
